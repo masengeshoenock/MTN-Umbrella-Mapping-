daa216459616e4aa617b6a9cb188f415b0cc3496
masengeshoenock <enockmasengesho1@gmail.com>
2020-02-27 16:29:12 +0200

    my first system

---
+++ modules1.py
@@ -0,0 +1,16 @@
+# import modules
+# modules.greeting("jonathan")
+
+
+# import modules
+# a = modules.person1["name"]
+# b = modules.person1["age"]
+# c = modules.person1["country"]
+# print(a,b,c)
+
+
+from modules import person1
+print(person1["name"])
+print(person1["age"])
+print(person1["country"])
+
